80e1dc3f32aa530e354cbb19e92b90592ada8779
nathalia.sautchuk <nathalia.sautchuk@5be3edc6-a98c-11de-8ba5-91705c1537ed>
2009-11-12 02:57:41 +0000

    adicionando novas maquinas e alterações no diagrama das maquinas expressão e termo

---
+++ Hopper/src/br/usp/sintatico/MaquinaFator.java
@@ -0,0 +1,87 @@
+package br.usp.sintatico;
+
+import br.usp.lexico.Simbolo;
+import br.usp.maquinaestados.MaquinaEstados;
+import br.usp.maquinaestados.Transicao;
+
+/**
+ *
+ * @author nathalia
+ */
+public class MaquinaFator extends SubMaquina{
+
+    public MaquinaFator() {
+        estadoInicial = 0;
+
+        //tabelaTransicoes.add(new Transicao(0, 1, "fator", "chamaFator"));
+        //tabelaTransicoes.add(new Transicao(1, 2, "+", "ignora"));
+        //tabelaTransicoes.add(new Transicao(1, 2, "-", "ignora"));
+        //tabelaTransicoes.add(new Transicao(3, 2, "fator", "chamaFator"));
+
+        tabelaEstadosAceitacao.add(1);
+
+        maquina = new MaquinaEstados(tabelaTransicoes, estadoInicial, tabelaEstadosAceitacao);
+    }
+
+    public boolean trataToken(Simbolo token) {
+        boolean retorno = false;
+
+        String acao = maquina.transita(token.getTipo());
+        if(acao.equals("ignora"))
+        {
+
+        }
+        else if(acao.equals("chamaFuncao"))
+        {
+            subMaquina = new MaquinaFuncao();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaDeclaracao"))
+        {
+            subMaquina = new MaquinaDeclaracao();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaEntrada"))
+        {
+            subMaquina = new MaquinaEntrada();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaSaida"))
+        {
+            subMaquina = new MaquinaSaida();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaCondicional"))
+        {
+            subMaquina = new MaquinaCondicional();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaIteracao"))
+        {
+            subMaquina = new MaquinaIteracao();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaAtribuicao"))
+        {
+            subMaquina = new MaquinaAtribuicao();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaIdentificador"))
+        {
+            subMaquina = new MaquinaIdentificador();
+            retorno = subMaquina.processaToken(token);
+        }
+        else if(acao.equals("chamaFator"))
+        {
+            subMaquina = new MaquinaFator();
+            retorno = subMaquina.processaToken(token);
+        }
+        else
+        {
+            System.out.println("Não existe ação definida!");
+        }
+
+        return retorno;
+    }
+
+}
