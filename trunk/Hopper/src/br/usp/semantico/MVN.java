39676fe6828b44aa503c45e00cece84fffa475a0
grisi.bruno <grisi.bruno@5be3edc6-a98c-11de-8ba5-91705c1537ed>
2009-11-28 16:44:37 +0000

    Classe básica da MVN (instrução 3 desconsiderada a pedido do RR).

---
+++ trunk/Hopper/src/br/usp/semantico/MVN.java
@@ -0,0 +1,73 @@
+package br.usp.semantico;
+
+/**
+ *
+ * @author Bruno Grisi
+ */
+public class MVN {
+    
+    public static String desvioIncondicional() {
+        return "0";
+    }
+    
+    public static String desvioAcumuladorZero() {
+        return "1";
+    }
+    
+    public static String desvioAcumuladorNegativo() {
+        return "2";
+    }
+
+	// Instrução Desconsiderada
+//    public static String constanteParaAcumulador() {
+//        return "3";
+//    }
+    
+    public static String soma() {
+        return "4";
+    }
+    
+    public static String subtracao() {
+        return "5";
+    }
+    
+    public static String multiplicacao() {
+        return "6";
+    }
+    
+    public static String divisao() {
+        return "7";
+    }
+    
+    public static String memoriaParaAcumulador() {
+        return "8";
+    }
+    
+    public static String acumuladorParaMemoria() {
+        return "9";
+    }
+    
+    public static String desvioParaSubprograma() {
+        return "A";
+    }
+    
+    public static String retornoDeSubPrograma() {
+        return "B";
+    }
+    
+    public static String parada() {
+        return "C";
+    }
+    
+    public static String entrada() { 
+        return "D";
+    }
+    
+    public static String saida() {
+        return "E";
+    }
+    
+    public static String chamadaSupervisor() {
+        return "F";
+    }
+}
